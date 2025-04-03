070fc92a2884a688370ce9c95d8e2abc12434b17
KNXCSdev <michal.miksho@gmail.com>
2025-04-03 23:59:03 +0200

    global styling

--- src/pages/CodingProblems.tsx
+++ src/pages/CodingProblems.tsx
@@ -1,3 +1,15 @@
+import Table from "../components/coding-problems/Table";
+import Header from "../components/header/Header";
+import SortArea from "../ui/SortArea";
+
 export default function CodingProblems() {
-  return <div>Coding Problems</div>;
+  return (
+    <>
+      <div className="mx-auto max-w-[120rem]">
+        <Header />
+        <SortArea />
+      </div>
+      <Table />
+    </>
+  );
 }
